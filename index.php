e1e1d49b301e6e7df0fdd3f651fd310fca0b2dba
Gabr19 <sannagabriele19@gmail.com>
2023-02-17 19:42:09 +0100

    inizializzazione

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $text ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem deserunt itaque harum architecto tempore dolorum veniam unde, numquam voluptas saepe?";
+    
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <?php echo $text?></h1>
+    <p><?php echo strlen($text)?></p>
+    
+</body>
+</html>
